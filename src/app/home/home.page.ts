c60d7e6d52474a0d741d21f743bb2c659d3d2bd5
wuzhinan@ThinkPadX1 <w.zhinan@gmail.com>
2022-06-23 09:26:47 +0800

    initial refresh

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { createWorker } from 'tesseract.js'
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,54 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor() {
+    this.loadWorker();
+  }
+
+  worker: Tesseract.Worker;
+  workerReady = false;
+  image = 'assets/t1.png';
+  ocrResult ="";
+  captureProgress = 0;
+  
+  async loadWorker()
+  {
+    this.worker = createWorker({
+      workerPath: 'assets/worker.min.js',
+      corePath: 'assets/tesseract-core.wasm.js',
+      langPath: 'assets/traineddata',
+      logger: progress => {
+        console.log(progress);
+        if(progress.status == 'recognizing text')
+        {
+          this.captureProgress = parseInt('' + progress.progress * 100);
+        }
+      }
+    });
+    await this.worker.load();
+    await this.worker.loadLanguage('eng');
+    await this.worker.initialize('eng');
+    this.workerReady = true;
+  }
+  
+  async captureImage(){
+    const image = await Camera.getPhoto({
+        quality:90,
+        allowEditing:true,
+        resultType: CameraResultType.DataUrl,
+        source: CameraSource.Camera
+    });
+
+    this.image = image.dataUrl;
+
+  }
+
+  async recognizeImage(){
+    // this.workerReady = false;
+    const result = await this.worker.recognize(this.image);
+    // this.workerReady = true;
+    this.ocrResult = result.data.text;
+    console.log(this.ocrResult);
+  }
 
 }
